c28eecb764e234456899f9d61e0e860eada8d972
AkhilJose <67900339+Akhil-Jo@users.noreply.github.com>
2023-11-29 11:32:42 +0530

    Update autoScroll.js

--- js/autoScroll.js
+++ js/autoScroll.js
@@ -1,6 +1,54 @@
 $(document).ready(function() {
-    // Scroll to the top when the page loads or refreshes
-    $('html, body').animate({
-        scrollTop: 0
-    }, 'slow');
+    // Check if the page has been refreshed
+    var pageRefreshed = localStorage.getItem('pageRefreshed');
+
+    // Function to scroll to the top
+    function scrollToTop() {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 'slow');
+    }
+
+    // If the page has been refreshed, scroll to the top
+    if (pageRefreshed) {
+        scrollToTop();
+    }
+
+    // Set a flag to indicate that the page has been refreshed
+    localStorage.setItem('pageRefreshed', true);
+});
+
+$(document).ready(function() {
+
+    // Function to scroll down slowly to div2, div3, and then to the bottom of the page
+    function slowScrollToDiv() {
+        var div2 = document.getElementById('fh5co-couple');
+        var div3 = document.getElementById('fh5co-event');
+        var currentPosition = window.scrollY;
+        var targetPosition;
+
+        // Determine the target position based on the current scroll position
+        if (currentPosition < div2.offsetTop) {
+            targetPosition = div2.offsetTop;
+        } else if (currentPosition < div3.offsetTop) {
+            targetPosition = div3.offsetTop;
+        } else {
+            // Clear the interval when it reaches div3
+            clearInterval(scrollInterval);
+            return;
+        }
+
+        var increment = 2; // Adjust the scrolling speed
+
+        if (currentPosition < targetPosition) {
+            // Scroll down
+            window.scrollTo(0, currentPosition + increment);
+        } else {
+            // Scroll up
+            window.scrollTo(0, currentPosition - increment);
+        }
+    }
+
+    // Set an interval for smooth scrolling after scrolling to the top
+    var scrollInterval = setInterval(slowScrollToDiv, 60); // Adjust the interval as needed
 });
